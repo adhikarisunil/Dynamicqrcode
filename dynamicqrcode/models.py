eca1de0370810876955c1c034918b648a553c46c
sunil <adhikarisunil339@gmail.com>
2023-05-18 20:51:59 +0545

    initialize

--- dynamicqrcode/models.py
+++ dynamicqrcode/models.py
@@ -10,32 +10,12 @@ class Dynamicqrcode(models.Model):
 
 
     name = models.CharField(max_length=255)
-
-    # ENGLISH = "E"
-    # NEPALI = "N"
-    # BOOK_CHOICES = [
-    #     (ENGLISH, "English"),
-    #     (NEPALI, "Nepali"),
-       
-    # ]
-    # type = models.ChoiceField(
-    #     max_length=1,
-    #     choices=BOOK_CHOICES,
-    #     default=ENGLISH,
-    
-    # )
-    # def is_upperclass(self):
-    #     return self.book_choice in {self.ENGLISH, self.NEPALI}
-
-
     QR_CODE_TYPE = Choices('English', 'Nepali')
     qr_code_type = StatusField(choices_name='QR_CODE_TYPE')
     content = models.JSONField(max_length=255)
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
-
-
 
     def __str__(self):
       return self.title
--- dynamicqrcode/viewsets.py
+++ dynamicqrcode/viewsets.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets
 from .models import Dynamicqrcode
 from .serializers import DynamicqrcodeSerializer
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import filters
 
 # ViewSets define the view behavior.
 class DynamicqrcodeModelViewSet(viewsets.ModelViewSet):
     queryset = Dynamicqrcode.objects.all()
     serializer_class = DynamicqrcodeSerializer
+    # permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['name', 'qr_code_type']
+    search_fields = ['name', 'qr_code_type']
